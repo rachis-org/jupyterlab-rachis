3be8e01a3b3cb69d4d1c0b874fbc7fe4a38d7a01
Evan Bolyen <ebolyen@gmail.com>
2026-02-17 17:32:20 +0100

    TST: adds playwright test

--- ui-tests/tests/jupyterlab-rachis.spec.ts
+++ ui-tests/tests/jupyterlab-rachis.spec.ts
@@ -1,51 +1,16 @@
 import { expect, test } from '@jupyterlab/galata';
 
 test('should display rachis-archive data file', async ({ page }) => {
-  const filename = 'test.qzv';
-  await page.menu.clickMenuItem('File>New>Text File');
 
-  // Set MIME type content in fill
-  await page.getByRole('main').getByRole('textbox').fill('');
-
-  await page.menu.clickMenuItem('File>Save Text');
-
-  await page.locator('.jp-Dialog').getByRole('textbox').fill(filename);
-
-  await page.getByRole('button', { name: 'Rename' }).click();
-  await page.waitForTimeout(200);
-
-  // Close file opened as editor
-  await page.activity.closePanel('test.my_type');
-
-  await page.filebrowser.open(filename);
+  await page.getByRole('menuitem', { name: 'File' }).click();
+  await page.getByText('Open from URL…').click();
+  await page.getByRole('textbox', { name: 'URL' }).click();
+  await page.getByRole('textbox', { name: 'URL' }).fill('https://raw.githubusercontent.com/qiime2/q2-fmt/master/demo/raincloud-baseline0.qzv');
+  await page.getByRole('button', { name: 'Open' }).click();
+ 
+  await page.waitForTimeout(4000);
 
   const view = page.getByRole('main').locator('.mimerenderer-rachis-archive');
 
   expect(await view.screenshot()).toMatchSnapshot('rachis-archive-file.png');
-});
-
-test('should display notebook rachis-archive output', async ({ page }) => {
-  await page.menu.clickMenuItem('File>New>Notebook');
-
-  await page.getByRole('button', { name: 'Select' }).click();
-
-  await page.notebook.setCell(
-    0,
-    'code',
-    `from IPython.display import display
-# Example of MIME type content
-output = {
-    "application/vnd.rachis.archive+zip": ""
-}
-
-display(output, raw=True)`
-  );
-
-  await page.notebook.run();
-
-  const outputs = page
-    .getByRole('main')
-    .locator('.mimerenderer-rachis-archive.jp-OutputArea-output');
-
-  await expect(outputs).toHaveCount(1);
 });
