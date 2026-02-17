338a1b81e2cbe41ed1ee4c7774efea831cfd3c36
Evan Bolyen <ebolyen@gmail.com>
2026-02-17 18:03:44 +0100

    lint

--- ui-tests/tests/jupyterlab-rachis.spec.ts
+++ ui-tests/tests/jupyterlab-rachis.spec.ts
@@ -1,13 +1,16 @@
 import { expect, test } from '@jupyterlab/galata';
 
 test('should display rachis-archive data file', async ({ page }) => {
-
   await page.getByRole('menuitem', { name: 'File' }).click();
   await page.getByText('Open from URL…').click();
   await page.getByRole('textbox', { name: 'URL' }).click();
-  await page.getByRole('textbox', { name: 'URL' }).fill('https://raw.githubusercontent.com/qiime2/q2-fmt/master/demo/raincloud-baseline0.qzv');
+  await page
+    .getByRole('textbox', { name: 'URL' })
+    .fill(
+      'https://raw.githubusercontent.com/qiime2/q2-fmt/master/demo/raincloud-baseline0.qzv'
+    );
   await page.getByRole('button', { name: 'Open' }).click();
- 
+
   await page.waitForTimeout(4000);
 
   const view = page.getByRole('main').locator('.mimerenderer-rachis-archive');
